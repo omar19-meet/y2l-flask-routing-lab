33f6796e88573efa438e362718f2880645e9d571
Omar Dahleh <omardahleh@Omars-MacBook-Air-2.local>
2018-10-25 18:54:46 +0300

    lol

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+    return render_template("home.html")
+
+@app.route('/shop')
+def shop_page():
+	cats=['https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350']
+	return render_template("shop.html",cats=cats)
 
 if __name__ == '__main__':
    app.run(debug = True)
